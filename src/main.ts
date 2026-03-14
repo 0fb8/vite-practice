7d4ddb912a7774ae6a0bbfc5956b81b700aed967
0fb8 <218385633+0fb8@users.noreply.github.com>
2026-03-15 00:48:04 +0900

    feat: count characters

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import "./style.css";
+
+const textarea = document.querySelector<HTMLTextAreaElement>("#text-input")!;
+const countDisplay = document.querySelector<HTMLSpanElement>("#count")!;
+
+textarea.addEventListener("input", () => {
+  const length = textarea.value.length;
+  countDisplay.textContent = length.toString();
+});
